9db3245351722bacaa3e4c3b7f14bccd42137f8c
NesterenkoAlexey <alechebil@mail.ru>
2021-08-17 17:09:18 +0300

    One more change +++

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,18 @@
+
 def mvn = "/var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/3.6.3/bin/mvn"
 
 pipeline {
-    agent any
-    stages {
 
+    agent {node { label 'linux'}}
+    stages {
         stage('Build') {
             steps {
-                sh "${mvn} clean compile"
+                sh "${mvn} clean install"
             }
         }
         stage('Run Tests') {
             steps {
-                sh "${mvn} test -Dcucumber.filter.tags=\"@firstTest\""
+                sh "${mvn} test -Dcucumber.filter.tags=\"${TAG}\""
             }
         }
         stage('Allure Report Generation') {
@@ -21,10 +22,6 @@ pipeline {
                         results: [[path: 'target/reports/allure-results']]
             }
         }
-    }
-    post {
-        always {
-            cleanWs notFailBuild: true
-        }
+
     }
 }
